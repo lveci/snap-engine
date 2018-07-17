1e49bdbb3d8d964f5786253bf9623e01ec2df9f8
lveci <luis@skywatch.co>
2018-07-17 10:15:56 -0400

    temp update no data value

--- snap-ndvi/src/main/java/org/esa/snap/ndvi/NdviOp.java
+++ snap-ndvi/src/main/java/org/esa/snap/ndvi/NdviOp.java
@@ -83,7 +83,7 @@ public class NdviOp extends Operator {
             rasterDataNodeType = Band.class)
     private String nirSourceBand;
 
-    private static final float nodatavalue = Float.NaN;
+    private static final float nodatavalue = -99;//Float.NaN;
 
     @Override
     public void initialize() throws OperatorException {
